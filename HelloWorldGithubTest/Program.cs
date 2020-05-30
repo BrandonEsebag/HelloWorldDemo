865ae61a75cfe268230aec562db7b707bbf6ed9c
Brandon Isaac Esebag <66148509+BrandonEsebag@users.noreply.github.com>
2020-05-30 12:19:00 -0700

    This code will get the system name and IP adress
    The file name is the same as the other branch but it does something else

---
+++ HelloWorldGithubTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry iph; //Device which IP is getting found
+            string myip = ""; //An empty string, for now
+            iph = Dns.GetHostEntry(Dns.GetHostName()); //This gets the IP Adress(s)
+            foreach(IPAddress ip in iph.AddressList) //Check if each one is the right type
+            {
+                if(ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) //Tom my understanding this family connects to an endpoint, like a modem or pc
+                {
+                    myip = ip.ToString(); //The one that is connected to the computer
+                }
+            }
+            Console.WriteLine("Your Device's name is " + Dns.GetHostName() + " and it's IP Adress it" + myip); //Write the IP out.
+            Console.ReadLine();
+        }
+    }
+}
