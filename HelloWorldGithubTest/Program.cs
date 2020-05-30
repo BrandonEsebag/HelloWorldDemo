a650e031830293f048679e3228ad3985b6b16e66
Brandon Isaac Esebag <66148509+BrandonEsebag@users.noreply.github.com>
2020-05-30 00:05:35 -0700

    This is a simple hello world script
     - though I have no idea which files are actually needed from the folder
    
    Here is the code:
    using System;
    
    namespace HelloWorldGithubTest
    {
        class Program
        {
            static void Main(string[] args)
            {
                //This is the code demo requested for Matthew. The code was generated when I made the project, but I am pretty sure the main goal of this exersize was to learn how to use GitHub, so i will try to upload this.
                Console.WriteLine("Hello World!");
            }
        }
    }

---
+++ HelloWorldGithubTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the code demo requested for Matthew. The code was generated when I made the project, but I am pretty sure the main goal of this exersize was to learn how to use GitHub, so i will try to upload this.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
