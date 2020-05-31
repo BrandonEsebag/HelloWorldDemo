234ced616df1384c8d40e87d84b5f60dda11248e
Brandon Isaac Esebag <66148509+BrandonEsebag@users.noreply.github.com>
2020-05-30 19:06:47 -0700

    This one gives the country as well
    Same as other API brach, but also gives the country

---
+++ HelloWorldGithubTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HelloWorldGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync();
+            Console.ReadLine();
+        }
+        static async void MainAsync()
+        {
+            var httpClient = new HttpClient();
+            var ip = await httpClient.GetStringAsync("https://api.myip.com/");
+            ip = ip.Replace("{\"ip\":\"", "Your IP Adress is ");
+            ip = ip.Replace("\",\"country\":\""," and falls within (the) ");
+            ip = ip.Substring(0, ip.Length - 12);
+            Console.WriteLine(ip);
+        }
+    }
+}
