31dd334b703c73af2f861649ad15e3fc9cd0c3d7
Brandon Isaac Esebag <66148509+BrandonEsebag@users.noreply.github.com>
2020-05-30 18:17:15 -0700

    API based IP Address
    This is the same as the other branch but uses the 'ipify' API to derive the IP address instead of the local machine.

---
+++ HelloWorldGithubTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HelloWorldGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync();
+            Console.ReadLine();
+        }
+        static async void MainAsync()
+        {
+            var httpClient = new HttpClient();
+            var ip = await httpClient.GetStringAsync("https://api.ipify.org");
+            Console.WriteLine($"My public IP address is: {ip}");
+        }
+    }
+}
